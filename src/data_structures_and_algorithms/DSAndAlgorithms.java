fe32c1f2c1525e436ed159581c36710d9d22526b
İsmail <ismail.demirler@gmail.com>
2021-10-24 16:50:01 +0300

    Introduction To DS

---
+++ src/data_structures_and_algorithms/DSAndAlgorithms.java
@@ -0,0 +1,19 @@
+package data_structures_and_algorithms;
+
+public class DSAndAlgorithms {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public static void introductionToDataStructures() {
+		
+		/*
+		 * Data Structures
+		 * 
+		 * 
+		 */
+		
+	}
+}
