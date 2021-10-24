f1e363b2381ea405244156e0d701d5c9235a59db
İsmail <ismail.demirler@gmail.com>
2021-10-24 21:36:33 +0300

    Analysis of Algorithms

--- src/data_structures_and_algorithms/DSAndAlgorithms.java
+++ src/data_structures_and_algorithms/DSAndAlgorithms.java
@@ -84,7 +84,7 @@ public class DSAndAlgorithms {
 	                   return n * (n + 1) / 2;
 			       }
 	               
-			    2) public int findSum(int n) {
+                2) public int findSum(int n) {
 	                   int sum = 0;
                        for(int i = 1; i <= n; i++) {
                            sum = sum + i;
